bdc31e8a310961e976d07848b893243e00c95f28
Erickcoelho20 <159166164+Erickcoelho20@users.noreply.github.com>
2024-02-10 19:13:40 -0300

    Add files via upload

---
+++ aside.php
@@ -0,0 +1,90 @@
+<aside class="left-sidebar" data-sidebarbg="skin5">
+    <!-- Sidebar scroll-->
+    <div class="scroll-sidebar">
+      <!-- Sidebar navigation-->
+      <nav class="sidebar-nav">
+        <ul id="sidebarnav" class="pt-4">
+          <li class="sidebar-item">
+            <a
+              class="sidebar-link waves-effect waves-dark sidebar-link"
+              href="../"
+              aria-expanded="false"
+              ><i class="mdi mdi-view-dashboard"></i
+              ><span class="hide-menu">Página inicial</span></a
+            >
+          </li>
+          <li class="sidebar-item">
+            <a
+              class="sidebar-link waves-effect waves-dark sidebar-link"
+              href="../usuarios"
+              aria-expanded="false"
+              ><i class="mdi mdi-account"></i
+              ><span class="hide-menu">Usuários</span></a
+            >
+          </li>
+          <li class="sidebar-item">
+            <a
+              class="sidebar-link waves-effect waves-dark sidebar-link"
+              href="../depositos"
+              aria-expanded="false"
+              ><i class="mdi mdi-cash"></i
+              ><span class="hide-menu">Depositos</span></a
+            >
+          </li>
+          <li class="sidebar-item">
+            <a
+              class="sidebar-link waves-effect waves-dark sidebar-link"
+              href="../saques"
+              aria-expanded="false"
+              ><i class="mdi mdi-cash"></i
+              ><span class="hide-menu">Saques</span></a
+            >
+          </li>
+          
+           <li class="sidebar-item">
+            <a
+              class="sidebar-link waves-effect waves-dark sidebar-link"
+              href="../pixels"
+              aria-expanded="false"
+              ><i class="mdi mdi-code-tags"></i
+              ><span class="hide-menu">Pixels</span></a
+            >
+          </li>
+          
+          
+                    <li class="sidebar-item">
+            <a
+              class="sidebar-link waves-effect waves-dark sidebar-link"
+              href="../gateway"
+              aria-expanded="false"
+              ><i class="mdi mdi-cash"></i
+              ><span class="hide-menu">Gateway</span></a
+            >
+          </li>
+          
+                    <li class="sidebar-item">
+            <a
+              class="sidebar-link waves-effect waves-dark sidebar-link"
+              href="../config"
+              aria-expanded="false"
+              ><i class="mdi mdi-account"></i
+              ><span class="hide-menu">Configurações</span></a
+            >
+          </li>
+          
+           <li class="sidebar-item">
+                            <a
+                              class="sidebar-link waves-effect waves-dark sidebar-link"
+                              href="../planos"
+                              aria-expanded="false"
+                              ><i class="mdi mdi-square-inc-cash"></i
+                              ><span class="hide-menu">Afiliados</span></a
+                            >
+                        </li>
+        
+        </ul>
+      </nav>
+      <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+ </aside>
